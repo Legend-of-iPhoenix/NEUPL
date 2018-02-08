f74698c3d48cea50f7e643ebfa1523827d71abe9
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2018-02-08 17:07:41 -0500

    Small changes.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,9 @@
-function showFunDebugThings() {
-  //override this function with the following code to show debug things.
-  return false;
-}
-
-// creates an overcomplicated NEUPL program that outputs a given string. Just for demo purposes, but I'm leaving it in the code for now.
-function NEUPLify(string) {
-  if (string) {
-    document.getElementById("input").value = string.split("").reverse().join("").split("").join("<<").replace(/ <</g, " ") + "<<l<p<le";
-  }
-}
+var doFunDebugThings = false;
 
 window.onload = function () {
   document.getElementById("input").value = decodeURI(location.search.match(/\?c\=([^\&]*)/)[1]);
+  tgif = Date().toLocaleString().startsWith("F");
+  console.log("   _   _ ______ _    _ _____  _        \n  | \\ | |  ____| |  | |  __ \\| |       \n  |  \\| | |__  | |  | | |__) | |       \n  | . ` |  __| | |  | |  ___/| |       \n  | |\\  | |____| |__| | |    | |____   \n  |_| \\_|______|\\____/|_|    |______|  \n                                       \n=======================================\n        Welcome to the console!        \n                                       \n    If you are a developer and want    \n    to contribute code and improve     \n    NEUPL, you can visit our GitHub    \n    repo, which is linked below.       \n                                       \n    If you are just poking around, we  \n    recommend checking out /js/main.js \n    first. It contains the function    \n    executeProgram() which interprets  \n    the NEUPL code entered by users.   \n=======================================\nhttps://github.com/Legend-of-iPhoenix/NEUPL"+(tgif?"\n P.S. Happy Friday":""));
 }
 
 function executeProgram() {
@@ -37,78 +29,80 @@ function executeProgram() {
     while (programCounter < programArray.length) {
       lastChar = curChar;
       curChar = programArray[programCounter].toLowerCase();
-      if (showFunDebugThings()) {
-        output.innerText += '[' + programCounter + '][' + curChar + '][' + TheStack.join('') + ']\n';
-      }
-      if (curChar == '"' && lastChar != '\\') {
-        inString = !inString;
-      } else {
-        if (inString && !(programArray[Math.min(programCounter + 1, 0)] == '"' && curChar == '\\')) {
-          TheStack.push(programArray[programCounter]);
+      if (curChar !== "<" && programArray[Math.min(programCounter + 1, 0)] !== "<") {
+        if (curChar == '"' && lastChar != '\\') {
+          inString = !inString;
         } else {
-          if (curChar == '<') {
-            TheStack.push(programArray[Math.max(programCounter - 1, 0)]);
-          }
-
-          if (curChar === ' ') {
-            TheStack.push(' ');
-          }
-
-          if (curChar == 'r') {
-            var nextChar = programArray[Math.min(programCounter + 1, programArray.length - 1)];
-            if (nextChar == 's') {
-              TheStack = TheStack.reverse();
-            } else {
-              /* disfunctional >_> if (nextChar == 'p') {
-                program = programArray.reverse().join('');
-                programArray = programArray.reverse();
-              }*/
+          if (inString && !(programArray[Math.min(programCounter + 1, 0)] == '"' && curChar == '\\')) {
+            TheStack.push(programArray[programCounter]);
+          } else {
+            if (curChar == '<') {
+              TheStack.push(programArray[Math.max(programCounter - 1, 0)]);
             }
-          }
 
-          if (curChar == 'p') {
-            var nextChar = programArray[Math.min(programCounter + 1, programArray.length - 1)];
-            if (nextChar == 'p') {
-              doImplicitPrint = false;
-              programCounter++;
-              output.innerText += TheStack.pop() + '\n';
-            } else {
-              if (nextChar == 'i') {
-                programCounter++;
-                program = program.slice(0, programCounter + 1) + TheStack.pop() + program.slice(programCounter + 1);
-                programArray = program.split('');
+            if (curChar === ' ') {
+              TheStack.push(' ');
+            }
+
+            if (curChar == 'r') {
+              var nextChar = programArray[Math.min(programCounter + 1, programArray.length - 1)];
+              if (nextChar == 's') {
+                TheStack = TheStack.reverse();
               } else {
-                if (nextChar == 'n')
-                  TheStack.pop();
-                programCounter++;
+                /* disfunctional >_> if (nextChar == 'p') {
+                  program = programArray.reverse().join('');
+                  programArray = programArray.reverse();
+                }*/
               }
             }
-          }
 
-          if (curChar == 'l') {
-            var nextChar = programArray[Math.min(programCounter + 1, programArray.length - 1)];
-            if (nextChar == 'p') {
-              doImplicitPrint = false;
-              programCounter++;
-              output.innerText += TheStack.reverse().join('') + '\n';
-              TheStack = [];
-            } else {
-              if (nextChar == 'e') {
-                programCounterStack.push(programCounter + 1)
-                programStack.push(program);
-                programCounter = -1;
-                program = TheStack.join('');
-                programArray = TheStack;
-                programLevel++;
+            if (curChar == 'p') {
+              var nextChar = programArray[Math.min(programCounter + 1, programArray.length - 1)];
+              if (nextChar == 'p') {
+                doImplicitPrint = false;
+                programCounter++;
+                output.innerText += TheStack.pop() + '\n';
+              } else {
+                if (nextChar == 'i') {
+                  programCounter++;
+                  program = program.slice(0, programCounter + 1) + TheStack.pop() + program.slice(programCounter + 1);
+                  programArray = program.split('');
+                } else {
+                  if (nextChar == 'n')
+                    TheStack.pop();
+                  programCounter++;
+                }
+              }
+            }
+
+            if (curChar == 'l') {
+              var nextChar = programArray[Math.min(programCounter + 1, programArray.length - 1)];
+              if (nextChar == 'p') {
+                doImplicitPrint = false;
+                programCounter++;
+                output.innerText += TheStack.reverse().join('') + '\n';
                 TheStack = [];
               } else {
-                if (nextChar == 'n') {
+                if (nextChar == 'e') {
+                  programCounterStack.push(programCounter + 1)
+                  programStack.push(program);
+                  programCounter = -1;
+                  program = TheStack.join('');
+                  programArray = TheStack;
+                  programLevel++;
                   TheStack = [];
+                } else {
+                  if (nextChar == 'n') {
+                    TheStack = [];
+                  }
                 }
               }
             }
           }
         }
+      }
+      if (showFunDebugThings {
+        output.innerText += '[' + programCounter + '][' + curChar + '][' + TheStack.join('') + ']\n';
       }
       programCounter++;
     }
@@ -124,3 +118,4 @@ function executeProgram() {
     output.innerText += TheStack.reverse().join('') + '\n';
   }
 }
+var tgif = "";
